2ded57b625ab94db4b34ef7010684ca8cb9f62bf
Enis Keskin <enis-1998@hotmail.com>
2022-03-29 00:10:56 +0300

    Add project files.

---
+++ WebApi/BookOperations/GetBookDetails/GetBookDetailValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.BookOperations.GetBookDetails
+{
+    public class GetBookDetailValidator : AbstractValidator<GetBookDetail>
+    {
+        public GetBookDetailValidator()
+        {
+            RuleFor(query => query.BookId).GreaterThan(0);
+        }
+    }
+}
---
+++ WebApi/BookOperations/UpdateBook/UpdateBookCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.DbOperations;
+
+namespace WebApi.BookOperations
+{
+    public class UpdateBookCommand
+    {
+        public UpdatedBookModel Model { get; set; }
+        public int BookId { get; set; }
+        private readonly BookStoreDbContext _dbContext;
+        public UpdateBookCommand(BookStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Handle()
+        {
+            var book = _dbContext.Books.Where(book => book.Id == BookId).SingleOrDefault();
+            //var book = _dbContext.Books.SingleOrDefault(x => x.Id == id);
+
+            if (book is null)
+            {
+                throw new InvalidOperationException("Book does not exist");
+            }
+            book.GenreId = Model.GenreId != default ? Model.GenreId : book.GenreId;
+            book.PageCount = Model.PageCount != default ? Model.PageCount : book.PageCount;
+            book.PublishDate = Model.PublishDate != default ? Model.PublishDate : book.PublishDate;
+            book.Title = Model.Title != default ? Model.Title : book.Title;
+
+            _dbContext.Books.Update(book);
+            _dbContext.SaveChanges();
+        }
+
+        public class UpdatedBookModel
+        {
+            public string Title { get; set; }
+            public int GenreId { get; set; }
+            public int PageCount { get; set; }
+            public DateTime PublishDate { get; set; }
+        }
+    }
+}
